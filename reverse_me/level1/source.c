0229e8531f50070b3372d831709bd58d7f2f6ce7
Eyzou <elodie.hamm@gmail.com>
2025-04-16 19:02:15 +0200

    reverser

---
+++ reverse_me/level1/source.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char input[423];
+    char* key = "__stack_check";
+    printf("Please enter key: ");
+    scanf("%s",(char*)&input);
+    if (strcmp(key,input))
+    {
+        printf("Nope.\n");
+        return(1);
+    }
+    else
+    {
+        printf("Good job.\n");
+        return 0;
+    }
+    return 0;
+}
---
+++ reverse_me/level2/source.c
@@ -0,0 +1,5 @@
+
+// - 2 0
+// commence par un d
+// convert ASCII number to caracter
+// then compare to the mdp
